1235e2bb075639bd4f651e0d0750d937245d7cd2
dkurp <31844775+dkurp@users.noreply.github.com>
2020-03-24 12:39:13 +0100

    basic calculator react (#3)

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import Application from "./Application";
+import Calculator from "./Calculator";
 import * as serviceWorker from "./serviceWorker";
 
 ReactDOM.render(
   <React.StrictMode>
-    <Application />
+    <Calculator />
   </React.StrictMode>,
   document.getElementById("root")
 );
